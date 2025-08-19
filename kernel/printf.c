3354310006e0b9bdbe17ac37af2810b45ae27e40
yurika971 <yurika971@gmail.com>
2025-08-19 21:23:35 +0800

    backtrace has been finished.

--- kernel/printf.c
+++ kernel/printf.c
@@ -132,3 +132,14 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void
+backtrace(void)
+{
+	printf("backtrace:\n");
+	uint64 pa = r_fp();
+	while (pa != PGROUNDUP(pa)){
+		printf("%p\n", *(uint64*)(pa - 8));
+		pa = *(uint64*)(pa - 16);
+	}
+}
