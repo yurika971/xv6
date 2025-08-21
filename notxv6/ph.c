194ead52848b4258441311414b837688df5041ce
yurika971 <yurika971@gmail.com>
2025-08-21 20:13:33 +0800

    ph has been finished.

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -8,6 +8,8 @@
 #define NBUCKET 5
 #define NKEYS 100000
 
+pthread_mutex_t lock;
+
 struct entry {
   int key;
   int value;
@@ -51,7 +53,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+		pthread_mutex_lock(&lock);
     insert(key, value, &table[i], table[i]);
+		pthread_mutex_unlock(&lock);
   }
 }
 
@@ -111,6 +115,9 @@ main(int argc, char *argv[])
   tha = malloc(sizeof(pthread_t) * nthread);
   srandom(0);
   assert(NKEYS % nthread == 0);
+
+	pthread_mutex_init(&lock, NULL);
+
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
