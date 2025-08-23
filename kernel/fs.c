649a71a9dfc9b3d024f17aba7d95b9036bdf775a
yurika971 <yurika971@gmail.com>
2025-08-23 16:06:23 +0800

    Lab: file system has been finished.

--- kernel/fs.c
+++ kernel/fs.c
@@ -377,7 +377,7 @@ iunlockput(struct inode *ip)
 static uint
 bmap(struct inode *ip, uint bn)
 {
-  uint addr, *a;
+  uint addr, *a, *b;
   struct buf *bp;
 
   if(bn < NDIRECT){
@@ -401,6 +401,29 @@ bmap(struct inode *ip, uint bn)
     return addr;
   }
 
+	bn -= NINDIRECT;
+	
+	if (bn < NINDIRECT * NINDIRECT){
+		if((addr = ip->addrs[NDIRECT + 1]) == 0)
+			ip->addrs[NDIRECT + 1] = addr = balloc(ip->dev);
+		bp = bread(ip->dev, addr);
+		a = (uint*)bp->data;
+		if((addr = a[bn / NINDIRECT]) == 0){
+      a[bn / NINDIRECT] = addr = balloc(ip->dev);
+      log_write(bp);
+    }
+		brelse(bp);
+		bp = bread(ip->dev, addr);
+		b = (uint*)bp->data;
+		if((addr = b[bn % NINDIRECT]) == 0){
+      b[bn % NINDIRECT] = addr = balloc(ip->dev);
+      log_write(bp);
+    }
+		brelse(bp);
+    return addr;
+	}
+
+
   panic("bmap: out of range");
 }
 
@@ -411,7 +434,7 @@ itrunc(struct inode *ip)
 {
   int i, j;
   struct buf *bp;
-  uint *a;
+  uint *a, *b;
 
   for(i = 0; i < NDIRECT; i++){
     if(ip->addrs[i]){
@@ -431,6 +454,26 @@ itrunc(struct inode *ip)
     bfree(ip->dev, ip->addrs[NDIRECT]);
     ip->addrs[NDIRECT] = 0;
   }
+
+	if (ip->addrs[NDIRECT + 1]){
+		bp = bread(ip->dev, ip->addrs[NDIRECT + 1]);
+		a = (uint*)bp->data;
+		for (i = 0; i < NINDIRECT; i++){
+			if (a[i]){
+				struct buf *bpp = bread(ip->dev, a[i]);
+				b = (uint*)bpp->data;
+				for (j = 0; j < NINDIRECT; j++){
+					if (b[j])
+						bfree(ip->dev, b[j]);
+				}
+				brelse(bpp);
+				bfree(ip->dev,a[i]);
+			}
+		}
+		brelse(bp);
+		bfree(ip->dev, ip->addrs[NDIRECT + 1]);
+		ip->addrs[NDIRECT + 1] = 0;
+	}
 
   ip->size = 0;
   iupdate(ip);
